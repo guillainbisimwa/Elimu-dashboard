b728696f0fd612a750c0d4c2eaf3c4d34b8ceadf
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2023-05-02 23:02:46 +0200

    Add default layouts

---
+++ src/layouts/dashboard/nav/config.js
@@ -0,0 +1,27 @@
+// component
+import SvgColor from '../../../components/svg-color';
+
+// ----------------------------------------------------------------------
+
+const icon = (name) => <SvgColor src={`/assets/${name}.svg`} sx={{ width: 1, height: 1 }} />;
+
+const navConfig = [
+  {
+    title: 'dashboard',
+    path: '/dashboard/app',
+    icon: icon('ic_analytics'),
+  },
+  {
+    title: 'doctor',
+    path: '/dashboard/doctor',
+    icon: icon('ic_analytics'),
+  },
+  {
+    title: 'patient',
+    path: '/dashboard/patient',
+    icon: icon('ic_analytics'),
+  },
+  
+];
+
+export default navConfig;
