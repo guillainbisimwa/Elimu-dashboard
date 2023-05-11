d2d802c06a64e9bfa6bfa7d3f2cce0eea29f5b9a
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2023-05-12 00:32:01 +0200

    Add Eleve Reducer

---
+++ src/redux/eleveReducer.js
@@ -0,0 +1,76 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// Here, we are using the createAsyncThunk function to create an asynchronous thunk to fetch 
+// the list of ecole. 
+// Then we define a new slice called eleveSlice with an initial state containing 
+// an empty list of ecole, isLoading flag, and an error message if any.
+
+export const fetchEleve = createAsyncThunk(
+  "eleve/fetchEleve",
+  async () => {
+    const response = await axios.get(
+      "https://elimu-backend.netlify.com/api/as"
+    );
+    return response.data;
+  }
+);
+
+const eleveSlice = createSlice({
+  name: "eleve",
+  initialState: {
+    eleveList: [
+      {
+        id: "123",
+        name :"Shabani",
+        phones : ['+2497855552'],
+        address :"Q. Mabanga Sud",
+        imgUrl : null,
+        parent: "123",
+        classe: "123",
+        timestamp: "2023-01-01",
+      }
+    ],
+    isLoadingEleve: false,
+    errorEleve: null,
+  },
+  reducers: {
+    addAnneScolaireStart: (state) => {
+      state.isLoadingEleve = true;
+    },
+    addEleveSuccess: (state, action) => {
+      state.isLoadingEleve = false;
+      state.eleveList = [ ...state.eleveList, {...action.payload }];
+      state.errorEleve = null;
+
+      // Store user data to LocalStorage
+      // localStorage.setItem('eleveList', JSON.stringify({ eleveList: action.payload }));
+    },
+    addEleveFailure: (state, action) => {
+      state.isLoadingEleve = false;
+      state.errorEleve = action.payload;
+    },
+  },
+  // In the extraReducers field, we define how the state should change when the asynchronous
+  // thunk fetchEleve is in a pending, fulfilled, or rejected state. 
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchEleve.pending, (state) => {
+        state.isLoadingEleve = true;
+        state.errorEleve = null;
+      })
+      .addCase(fetchEleve.fulfilled, (state, action) => {
+        state.isLoadingEleve = false;
+        state.eleveList = action.payload;
+        state.errorEleve = null;
+      })
+      .addCase(fetchEleve.rejected, (state, action) => {
+        state.isLoadingEleve = false;
+        state.errorEleve = action.errorEleve.message;
+      });
+  },
+});
+
+export const { addAnneScolaireStart, addEleveSuccess, addEleveFailure  } = eleveSlice.actions;
+
+export default eleveSlice.reducer;
