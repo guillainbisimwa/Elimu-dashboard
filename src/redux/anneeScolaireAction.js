524a49bf54a1b6e42e135affff5571e9542e2672
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2023-05-12 00:45:58 +0200

    Add Annee Classe Action

--- src/redux/anneeScolaireAction.js
+++ src/redux/anneeScolaireAction.js
@@ -2,12 +2,12 @@
 import { addAnneeScolaireFailure, addAnneeScolaireStart, addAnneeScolaireSuccess } from './anneeScolaireReducer';
 
 
-export const AnneScolaireAction = (nom, phone, address, email, website, imgUrl) => async (dispatch) => {
+export const AnneeScolaireAction = (nom) => async (dispatch) => {
   try {
     dispatch(addAnneeScolaireStart());
 
     // const response = await axios.post('https://elimu-api.herokuapp.com/api/v1/admin/auth/anneeScolaire', {
-    //   nom, phone, address, email, website, imgUrl
+    //   nom
     // });
 
     // Save user data to LocalStorage
@@ -16,7 +16,7 @@ export const AnneScolaireAction = (nom, phone, address, email, website, imgUrl) 
     // Dispatch loginSuccess action with response data
     // dispatch(addAnneeScolaireSuccess(response.data)); // TODO : USE NETWORK
 
-    dispatch(addAnneeScolaireSuccess({name: nom, phones: [...phone], address, email, website, imgUrl, id:"12355", timestamp: "2023-01-01"})) // TODO : Just for test!
+    dispatch(addAnneeScolaireSuccess({name: nom, id:"12355", timestamp: "2023-01-01"})) // TODO : Just for test!
 
   } catch (error) {
     console.log(error);
