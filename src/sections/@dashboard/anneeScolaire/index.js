12099464acd41f74f1b65121882b6f55e4bd920c
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2023-05-12 00:06:56 +0200

    Add AS list head

---
+++ src/sections/@dashboard/anneeScolaire/index.js
@@ -0,0 +1,3 @@
+export { default as AnneeScolaireListHead } from './AnneeScolaireListHead';
+export { default as AnneeScolaireListToolbar } from './AnneeScolaireListToolbar';
+export { default as AddAnneeScolaire } from './AddAnneeScolaire';
