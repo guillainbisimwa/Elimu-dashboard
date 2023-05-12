35d6694daaa234547eae46279e4b9eddc8bdc12e
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2023-05-12 09:27:51 +0200

    Add Communiation page

--- src/routes.js
+++ src/routes.js
@@ -14,6 +14,7 @@ import AnneeScolairePage from './pages/AnneeScolairePage';
 import ClassePage from './pages/ClassePage';
 import ElevePage from './pages/ElevePage';
 import PaymentPage from './pages/PaymentPage';
+import CommunicationPage from './pages/CommunicationPage';
 
 export default function Router() {
   const { user } = useSelector((state) => state.auth);
@@ -31,6 +32,7 @@ export default function Router() {
         { path: 'classe', element: <ClassePage /> },
         { path: 'eleve', element: <ElevePage /> },
         { path: 'payment', element: <PaymentPage /> },
+        { path: 'communication', element: <CommunicationPage /> },
       ],
     },
     {
