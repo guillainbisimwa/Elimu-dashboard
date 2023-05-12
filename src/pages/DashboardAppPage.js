b12841b7cad811fc9a8489fbc5880aefded5728f
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2023-05-12 10:35:13 +0200

    Update dashboard

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -7,8 +7,10 @@ import {
   AppWidgetSummary,
 } from '../sections/@dashboard/app';
 import { store } from '../redux/Store';
-import { fetchEcole } from '../redux/ecoleReducer';
 import { fetchParents } from '../redux/parentsReducer';
+import { fetchEleve } from '../redux/eleveReducer';
+import { fetchCommunication } from '../redux/communicationReducer';
+import { fetchClasse } from '../redux/classeReducer';
 
 function countItems(ar) {
   return ar.length
@@ -16,13 +18,17 @@ function countItems(ar) {
 
 export default function DashboardAppPage() {
 
-  const { ecoleList } = useSelector((state) => state.ecole);
   const { parentList } = useSelector((state) => state.parents);
+  const { eleveList } = useSelector((state) => state.eleves);
+  const { classeList } = useSelector((state) => state.classes);
+  const { communicationList } = useSelector((state) => state.communications);
 
   useEffect(() => {
     // Fetch doctor and patient lists when component mounts
-    store.dispatch(fetchEcole());
     store.dispatch(fetchParents());
+    store.dispatch(fetchEleve());
+    store.dispatch(fetchCommunication());
+    store.dispatch(fetchClasse());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [store.dispatch]);
 
@@ -39,11 +45,19 @@ export default function DashboardAppPage() {
 
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Ecole" total={countItems(ecoleList)} icon={'ant-design:user-outlined'} />
+            <AppWidgetSummary title="Classe" total={countItems(classeList)} icon={'healthicons:i-training-class'} />
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Parents" total={countItems(parentList)} color="error" icon={'ant-design:user-outlined'} />
+            <AppWidgetSummary title="Parents" total={countItems(parentList)} color="error" icon={'ri:parent-line'} />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <AppWidgetSummary title="Eleves" total={countItems(eleveList)} color="info" icon={'ph:student-light'} />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <AppWidgetSummary title="Communications" total={countItems(communicationList)} color="warning" icon={'material-symbols:communication'} />
           </Grid>
 
         </Grid>
